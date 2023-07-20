09c95fd51597909f6eab0972690047fc6f3c33b5
wingunkh <99gusrms@naver.com>
2023-07-20 12:01:01 +0900

    섹션 3. 스프링 핵심 원리 이해2 - 객체 지향 원리 적용 / 스프링으로 전환하기

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,7 +1,6 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
@@ -9,20 +8,27 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration // 스프링 컨테이너는 해당 어노테이션이 붙은 클래스를 설정(구성) 정보로 사용한다.
 public class AppConfig {
+    @Bean // 스프링 컨테이너는 해당 어노테이션이 붙은 메서드를 모두 호출해서 반환된 객체를 스프링 컨테이너에 등록한다.
     public MemberService memberService() {
-        return new MemberServiceImpl(memberRepository());
+        return new MemberServiceImpl(memberRepository()); // 이렇게 등록된 객체를 스프링 빈이라고 한다.
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), DiscountPolicy());
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public DiscountPolicy DiscountPolicy() {
         // return new FixDiscountPolicy();
         return new RateDiscountPolicy();

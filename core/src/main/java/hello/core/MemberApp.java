0fec526f90cd5233513c2ca9059c3be20f135135
wingunkh <99gusrms@naver.com>
2023-07-21 12:03:37 +0900

    섹션 4. 스프링 컨테이너와 스프링 빈 / 스프링 컨테이너 생성

--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -12,6 +12,9 @@ public class MemberApp {
         // MemberService memberService = appConfig.memberService();
 
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        // 스프링 컨테이너 생성
+        // 스프링 컨테이너는 파라미터로 넘어온 설정(구성) 정보 클래스를 사용해서 스프링 빈을 등록한다.
+        // 그 후, 스프링 컨테이너는 의존관계를 주입한다.
         MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
         Member member = new Member(1L, "memberA", Grade.VIP);

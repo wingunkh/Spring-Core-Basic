747dc8ece690ae3178326dcfcd580f0ad17177d3
wingunkh <99gusrms@naver.com>
2023-07-29 00:09:15 +0900

    섹션 4. 스프링 컨테이너와 스프링 빈 / 스프링 빈 설정 메타 정보 - BeanDefinition

---
+++ core/src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
@@ -0,0 +1,27 @@
+package hello.core.beandefinition;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class BeanDefinitionTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    // GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+
+    @Test
+    @DisplayName("빈 설정 메타 정보 확인")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+            // 스프링 컨테이너는 스프링 빈 설정 메타 정보를 BeanDefinition을 통해 추상화한다.
+
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                System.out.println("beanDefinitionName = " + beanDefinitionName + " beanDefinition = " + beanDefinition);
+            }
+        }
+    }
+}

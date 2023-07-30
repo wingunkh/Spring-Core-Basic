61bf5f66d71d2a3e1351ab3bc7dd867d48763d65
wingunkh <99gusrms@naver.com>
2023-07-30 11:55:30 +0900

    섹션 5. 싱글톤 컨테이너 / 웹 애플리케이션과 싱글톤

---
+++ core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,26 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SingletonTest {
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer() {
+        AppConfig appConfig = new AppConfig();
+
+        // 호출할 때 마다 다른 객체를 생성한다.
+        MemberService memberService1 = appConfig.memberService();
+        MemberService memberService2 = appConfig.memberService();
+
+        // 참조값이 다른 것을 확인할 수 있다.
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        // memberService1 != memberService2
+        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+    }
+}

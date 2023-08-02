5a76adef24e44ae64e8f91da28253d207d3ba16a
wingunkh <99gusrms@naver.com>
2023-08-02 12:15:42 +0900

    섹션 5. 싱글톤 컨테이너 / @Configuration과 바이트코드 조작의 마법

--- core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -28,4 +28,18 @@ public class ConfigurationSingletonTest {
         assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
         assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
     }
+
+    @Test
+    void configurationDeep() {
+        // 이 때 AppConfig도 스프링 빈으로 등록된다.
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
+        // AppConfig 클래스에 @Configuration 어노테이션을 사용한다면?
+        // bean = class hello.core.AppConfig$$EnhancerBySpringCGLIB$$4a5fc277 출력
+        // 스프링이 CGLIB 바이트코드 조작 라이브러리를 사용하여 AppConfig 클래스를 상속받은 임의의 클래스 생성 및 스프링 빈으로 등록
+        // 해당 클래스가 싱글톤 보장
+    }
 }

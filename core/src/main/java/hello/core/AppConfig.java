d82b76b1db9f5b43bf353b7937a243862ea6bb4e
wingunkh <99gusrms@naver.com>
2023-08-02 11:08:21 +0900

    섹션 5. 싱글톤 컨테이너 / @Configuration과 싱글톤

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -15,21 +15,24 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
     @Bean // 스프링 컨테이너는 해당 어노테이션이 붙은 메서드를 모두 호출해서 반환된 객체를 스프링 컨테이너에 등록한다.
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository()); // 이렇게 등록된 객체를 스프링 빈이라고 한다.
     }
 
     @Bean
     public OrderService orderService() {
-        return new OrderServiceImpl(memberRepository(), DiscountPolicy());
+        System.out.println("call AppConfig.orderService");
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
-    public DiscountPolicy DiscountPolicy() {
+    public DiscountPolicy discountPolicy() {
         // return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }

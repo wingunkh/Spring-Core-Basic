0976b26f8e88700a9aae1958d205ba7c891dbdb5
wingunkh <99gusrms@naver.com>
2023-08-10 14:51:30 +0900

    섹션 7. 의존관계 자동 주입 / 롬복과 최신 트렌드

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,19 +3,16 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
+// 최근에는 생성자를 딱 1개 두고 @AutoWired 어노테이션을 생략하는 방법을 주로 사용한다.
+// 여기에 Lombok 라이브러리의 @RequiredArgsConstructor 어노테이션을 함께 사용하면 코드를 깔끔하게 작성할 수 있다.
 public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {

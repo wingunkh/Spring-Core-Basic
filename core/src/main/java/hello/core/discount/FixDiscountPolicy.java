123a495f57cb0211e0826e98823c6de917bf5e31
wingunkh <99gusrms@naver.com>
2023-08-11 17:15:20 +0900

    섹션 7. 의존관계 자동 주입 / @Autowired 필드명, @Qualifier, @Primary

--- core/src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ core/src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -5,6 +5,7 @@ import hello.core.member.Member;
 import org.springframework.stereotype.Component;
 
 @Component
+// @Qualifier("fixDiscountPolicy")
 public class FixDiscountPolicy implements DiscountPolicy {
     private int discountFixAmount = 1000; // 1000원 할인
 
--- core/src/test/java/hello/core/scan/AutoAppConfigTest.java
+++ core/src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -15,4 +15,8 @@ public class AutoAppConfigTest {
         assertThat(memberService).isInstanceOf(MemberService.class);
     }
     // 타입 조회 시 선택된 빈이 2개 이상이기 때문에 NoUniqueBeanDefinitionException 발생
+    // 해결 방법
+    // 1. @Autowired 파라미터명 매칭
+    // 2. @Qualifier 어노테이션 (추가 구분자)
+    // 3. @Primary 어노테이션 (우선순위 지정)
 }

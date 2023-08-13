bb9c307b1e06bfe069506ccce2f04dca82feca1b
wingunkh <99gusrms@naver.com>
2023-08-13 18:49:00 +0900

    섹션 8. 빈 생명주기 콜백 / 애노테이션 @PostConstruct, @PreDestroy

--- core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -16,10 +16,7 @@ public class BeanLifeCycleTest {
 
     @Configuration
     static class LifeCycleConfig {
-        @Bean(initMethod = "init", destroyMethod = "close")
-        // 설정 정보 클래스에 초기화 메서드와 소멸 메서드를 지정할 수 있다.
-        // 이 방법은 스프링에 지나치게 의존적이던 직전의 인터페이스 방법의 단점을 보완하였지만 다음 방법인 어노테이션 방법을 제일 많이 사용한다.
-
+        @Bean
         // 스프링 빈의 이벤트 라이프 사이클
         // 스프링 컨테이너 생성 -> 스프링 빈 생성 -> 의존관계 주입 -> 초기화 콜백 -> 사용 -> 소멸 전 콜백 -> 스프링 컨테이너 종료
         public NetworkClient networkClient() {
--- core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package hello.core.lifecycle;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
     private String url;
 
@@ -23,12 +26,18 @@ public class NetworkClient {
         System.out.println("disconnect: " + url);
     }
 
+    @PostConstruct
+    // 최신 스프링에서 가장 권장하는 방법이다.
+    // javax 패키지 기술이다. 즉, 스프링에 종속적인 기술이 아니라 JSR-250이라는 자바 표준이기 때문에 스프링이 아닌 다른 컨테이너에서도 잘 동작한다.
+    // 유일한 단점은 외부 라이브러리에는 적용하지 못한다는 것이다. (코드를 변경해야 하므로)
+    // 위의 경우 직전의 방법 (초기화 메서드, 소멸 메서드 지정)을 사용한다.
     public void init() throws Exception {
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
+    @PreDestroy
     public void close() throws Exception {
         System.out.println("NetworkClient.close");
         disconnect();

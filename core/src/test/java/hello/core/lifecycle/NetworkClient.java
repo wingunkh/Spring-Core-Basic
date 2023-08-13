7b2bc0b6ae2e07bcc2b444e1daabe8e82dada07a
wingunkh <99gusrms@naver.com>
2023-08-13 18:40:09 +0900

    섹션 8. 빈 생명주기 콜백 / 빈 등록 초기화, 소멸 메서드

--- core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,11 +1,6 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-// InitializingBean 인터페이스는 초기화를, DisposableBean 인터페이스는 소멸을 지원한다.
-// 이 방법은 스프링에 지나치게 의존적이기 때문에 현재 거의 사용하지 않는다.
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
     private String url;
 
     public NetworkClient() {
@@ -28,15 +23,14 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("disconnect: " + url);
     }
 
-
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    public void init() throws Exception {
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception {
+    public void close() throws Exception {
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 }
